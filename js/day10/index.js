1cc4b761bc338d591acb873b4e0dd193a83bef83
Taisei <taisei.960420@gmail.com>
2025-04-10 06:14:35 +0900

    day10

---
+++ js/day10/index.js
@@ -0,0 +1,23 @@
+/*
+
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+
+*/
+
+var once = function (fn) {
+  let called = false;
+  return function (...args) {
+    if (!called) {
+      called = true;
+      // 初回呼び出し時は fn を実行して結果を返す
+      return fn(...args);
+    }
+    // 2回目以降は undefined を返す
+    return undefined;
+  }
+}
+
+
